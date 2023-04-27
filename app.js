4ffc4cfbfef8eeb7cb75953844192c920da25947
Artem Zubov <artmzbv@gmail.com>
2023-04-27 04:29:12 +0300

    mydb_rename

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://127.0.0.1:27017/mydb');
+mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
